1b501119a7cf38b22e2eab46634a0fa88feaff30
taehyunhwang72 <taehyunhwang72@gmail.com>
2018-04-25 21:40:18 +0900

    Navbar & Generalized Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,31 @@
 /* Wow Animated Effect */
 new WOW().init();
 
+/* Scroll to Top */
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scrolltoTop").style.display = "block";
+  } else {
+      document.getElementById("scrolltoTop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// ScrolltoTop smoothly
+$('#scrolltoTop').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+});
+
 /* Flex Panel Gallery */
 const panels = document.querySelectorAll('.panel');
 
